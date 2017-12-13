e8108edaa1f6728162dc7123c3f950b5239fa89d
Tim Freeman <d.timothy.freeman@gmail.com>
2017-12-13 13:13:44 -0500

    added images and text to about me section. much of the styling is done

--- src/components/MyStory.js
+++ src/components/MyStory.js
@@ -4,7 +4,27 @@ const MyStory = (props) => {
 
   return (
     <div id='my-story' className='container'>
-      .
+      <div id='growing-up'>
+        <img src={require('../images/tennessee.gif')} alt=""/>
+        <p>I grew up just outside of Nashville, TN and went to college at the University of Memphis...
+        </p>
+      </div>
+      <div id='chicago-madrid'>
+        <img id='chicago' src={require('../images/chicago.jpg')} alt=""/>
+        <img id='madrid' src={require('../images/madrid.jpg')} alt=""/>
+        <p>Then I taught high school for two years in Chicago and elementary school for one year in Madrid...</p>
+      </div>
+      <div id='nyc-time'>
+        <img id='nyc' src={require('../images/nyc.png')} alt=""/>
+        <p>I have lived in NYC since 2011, teaching high school in Harlem until July 2017.</p>
+      </div>
+      <div id='flatiron-time'>
+        <img id='code-pic' src={require('../images/code.jpg')} alt=""/>
+        <p>Now, I am a full stack software engineer seeking opportunities to grow and learn as a developer.</p>
+      </div>
+      <div>
+        
+      </div>
     </div>
   )
 }
